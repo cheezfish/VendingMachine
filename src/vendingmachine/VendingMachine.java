4f72f59dc3234fbff76b4fc9ef73f6260d566909
Imran <Imran@192.168.0.14>
2017-11-10 20:40:37 +0000

    commit

--- src/vendingmachine/VendingMachine.java
+++ src/vendingmachine/VendingMachine.java
@@ -4,45 +4,48 @@ import java.util.Scanner;
 /**
  * @author Imran Aziz 03/10/17 
  */
-public class VendingMachine {
+public class VendingMachine 
+{
 
     /**
      * @param args the command line arguments
      */
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
-        init(); 
-        selected(); 
+        int a = input(); 
+        selected(a); 
         enjoy(); 
+        System.exit(0);
         
     }
     
-    public static void init() { //Announces vending machine selections and asks for selection
-        
+    public static int input()
+    {
         System.out.println("What's cookin'?");
         System.out.println("1. Coffee" + " 2. Tea" + " 3. HotChoc" + " 4. Miso");
-        
-    }
-    
-    public static void selected() { //Takes input and uses if statements to determine output
-        
         System.out.println("Enter number to continue");
         Scanner scan = new Scanner(System.in);
-        String select = scan.nextLine();
-        if (select.equals("1"))
+        int select = scan.nextInt();
+        return select;
+    }
+    
+    public static void selected(int a) { //Takes input and uses if statements to determine output
+        
+        if (a == 1)
         {
             System.out.println("Enjoy your Coffee! That'll be 5.00");
         }   
-        else if (select.equals("2"))
+        if (a == 2)
         {
             System.out.println("Enjoy your Tea! That's 1.75");
         }
-        else if (select.equals("3"))
+        if (a == 3)
         {
             System.out.println("Enjoy your HotChoc! That'll be 2.50");
         }
-        else if (select.equals("4"))
+        if (a == 4)
         {
             System.out.println("Pfft, hipster. That's 3.75");
         }
